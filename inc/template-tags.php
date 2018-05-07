ef1e3b7240c427b85e19d234e580384c1c8e1860
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-05-08 02:26:09 +0600

    author by update

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -42,7 +42,7 @@ if ( ! function_exists( 'gamiphy_posted_by' ) ) :
 	function gamiphy_posted_by() {
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'gamiphy' ),
+			esc_html_x( ' %s', 'post author', 'gamiphy' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
