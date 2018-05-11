56885203ea0e121ae9c4c113c55371455e5a9537
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-05-11 20:35:20 +0600

    for validator added and script removed

--- footer.php
+++ footer.php
@@ -58,12 +58,6 @@
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
         crossorigin="anonymous"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/main.js"></script>
-    <script>
-    $(".nav-item").on("click", function () {
-        $(".navbar-nav").find(".active").removeClass("active");
-        $(this).addClass("active");
-    });
-</script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
 </body>
 </html>
