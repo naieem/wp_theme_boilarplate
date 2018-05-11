cbe41332ed8fd70f4d9dee212ac84ae71dc34d99
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-05-11 20:37:27 +0600

    main js introduced for using demo registration submit related coding

---
+++ js/main.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($) {
+	/**
+	 * On nav item click function
+	 */
+	$(".nav-item").on("click", function () {
+        $(".navbar-nav").find(".active").removeClass("active");
+        $(this).addClass("active");
+    });
+	/**
+	 * Function to fire on clicking to requesting demo form
+	 */
+	$(document).on( 'submit', 'form.demo-request-form', function(event) {
+		event.preventDefault();
+		
+		var data = $(this).serialize();
+		var resultContainerid = $(this).attr('data-response-id');
+		var resultContainer = $('#'+resultContainerid);
+		var currentForm = $(this);
+		resultContainer.html('<img class="alignnone" src="https://cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif" alt="" width="48" height="48">');
+		$.ajax({
+			url : site.ajax_url,
+			type : 'post',
+			data : data + "&action=add_demo_request",
+			success : function( response ) {
+				currentForm.trigger("reset");
+				resultContainer.html(response);
+				setTimeout(function(){
+					resultContainer.html('')
+				}, 3000);
+			}
+		});
+	});
+});
