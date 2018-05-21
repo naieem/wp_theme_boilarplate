81000862a3702ee9891b9a274a8bdac3693c4e2c
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-05-22 01:33:49 +0600

    calendly added in contact us form page

--- page-contact-us.php
+++ page-contact-us.php
@@ -52,6 +52,14 @@ endwhile; // End of the loop.
                     <div class="one"></div>
                     <div class="two"></div>
                 </div>
+                <div class="col-md-12 pl-5 pr-5">
+                <!-- Calendly inline widget begin -->
+                        <div class="calendly-inline-widget" data-url="<?php echo $options['calendly_url']; ?>" style="min-width:320px;height:580px;"></div>
+                            <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js">
+
+                            </script>
+                        <!-- Calendly inline widget end -->
+                <div>
             </div>
         </div>
     </section>
