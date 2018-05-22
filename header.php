af76c4aba58834b60461f0246c0333bfe7a6a624
ahmad alstaty <ahmad.alstaty@gmail.com>
2018-05-22 11:18:26 +0300

    try to fix the loading async of the style files

--- header.php
+++ header.php
@@ -43,10 +43,8 @@ if (!is_home()) {?>
         // load Google Web Font 
         loadCSS("<?php echo get_stylesheet_directory_uri(); ?>/assets/css/font-awesome/css/font-awesome.min.css" );
         loadCSS("https://fonts.googleapis.com/css?family=Raleway" );
+        loadCSS("https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" );
     </script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
-          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
-          crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/main.css">
     <?php wp_head();
 global $options;
